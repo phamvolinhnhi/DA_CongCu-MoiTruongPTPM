a4a953026b57d006bf56eb23e35386539cb49e87
Pham Vo Linh Nhi <phamvolinhnhi02@gmail.com>
2023-03-08 16:28:04 +0700

    layout header

---
+++ WebTruyen/WebTruyen/Migrations/202302240535595_addusersproperties.cs
@@ -0,0 +1,20 @@
+﻿namespace WebTruyen.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addusersproperties : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "FullName", c => c.String(nullable: false, maxLength: 100));
+            AddColumn("dbo.AspNetUsers", "Gender", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Gender");
+            DropColumn("dbo.AspNetUsers", "FullName");
+        }
+    }
+}
---
+++ WebTruyen/WebTruyen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTruyen.Startup))]
+namespace WebTruyen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
