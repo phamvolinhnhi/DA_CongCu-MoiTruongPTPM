eff8cdad00cb1afd9f4cf8b472c0edcd8213c0cf
I'mKien <113129593+ThiKien@users.noreply.github.com>
2023-03-19 13:55:41 +0700

    Add files via upload

---
+++ UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebSach.Models;
+
+namespace WebSach.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly Model1 context = new Model1();
+
+        // GET: User
+        private Model1 db = new Model1();
+
+     
+       
+
+        public ActionResult Profile()
+        {
+            return View();
+        }
+      
+
+      
+
+        public ActionResult Detail(string user)
+        {
+
+            if (String.IsNullOrEmpty(user))
+            {
+                return HttpNotFound();
+            }
+            User find = context.Users.FirstOrDefault(p => p.User_Name == user);
+            if (find == null)
+                return HttpNotFound();
+            return View(find);
+
+        }
+
+
+    }
+}
+   
+    
+
+   
+
